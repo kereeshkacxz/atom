cf5d9afb8f0bbd479a81e5ef3d38942f95a367d8
Viacheslav <kcv204@gmail.com>
2024-10-13 04:30:56 +0300

    shortly add

---
+++ backend/app/static/dictOfDecriptions.py
@@ -0,0 +1,27 @@
+descriptions_dict = {
+    'AVAS': """The regulation covers the certification of Quiet Road Transport Vehicles (QRTV) regarding their reduced audibility, requiring the installation of Acoustic Vehicle Alerting Systems (AVAS) in electrified vehicles for safety.
+
+Key Points
+- Vehicles must have an **AVAS** that meets the required sound levels.
+- Minimum and maximum sound limits: Vehicles must emit a sound between set levels to ensure audibility without exceeding 75 dB(A).
+- Frequency shift: The sound must change with vehicle speed to indicate acceleration or deceleration.
+- Pause function: Deactivating AVAS is not allowed.
+- Production compliance: Manufacturers must ensure all vehicles conform to the approved type.""",
+    'Braking': """This regulation governs the approval of braking systems for passenger cars in categories M1 and N1. It defines technical requirements for service, secondary, and parking braking systems, ensuring safety and performance across various driving conditions.
+
+Key Points
+Braking Systems: Must include service, secondary, and parking brakes, each fulfilling specific safety requirements.
+Performance Tests: Vehicles undergo rigorous tests to ensure braking efficiency under different loads and speeds.
+Wear Indicators: Systems must allow for easy assessment of brake component wear.
+Energy Sources: Vehicles must maintain braking performance even if energy sources fail.
+Production Compliance: Consistent quality control is required to maintain type approval​""",
+
+    "Wipe_and_wash": """This regulation applies to passenger cars' windscreen wiper and washer systems. It specifies the technical requirements and testing methods to ensure functionality and performance, particularly under various environmental conditions.
+
+Key Points
+
+Windscreen Wiper System: Must cover at least 80% of zone B and 98% of zone A of the windscreen and operate with at least two wiping speeds.
+Windscreen Washer System: Must effectively deliver liquid to the windscreen without leaks and withstand temperatures from -18°C to 80°C.
+Test Requirements: Systems must pass various tests for temperature extremes, performance during blockages, and resistance to wear​.""",
+
+}
